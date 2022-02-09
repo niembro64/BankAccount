c5d41acc91b7aa1c3023181a4d4f0ba463e5e2bb
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-02-09 11:00:06 -0500

    Halfway

--- BankAccount.py
+++ BankAccount.py
@@ -1,17 +1,27 @@
 
 
 class BankAccount:
-    # don't forget to add some default values for these parameters!
-    def __init__(self, int_rate, balance): 
-        # your code here! (remember, instance attributes go here)
-        # don't worry about user info here; we'll involve the User class soon
+
+    # don't forget to add some default values for these parameters!
+    def __init__(self, n = "", i_r = 0, b = 0):
+        self.name = n
+        self.balance = b
+        self.int_rate = i_r
+        
+        # your code here! (remember, instance attributes go here)
+        # don't worry about user info here; we'll involve the User class soon
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+    def withdraw(self, amount):
+        self.balance -= amount
+        return self
+    def display_account_info(self):
+        print(f"{self.name} : {self.balance} : {self.int_rate}")
+        return self
+    def yield_interest(self):
+        self.balance = self.balance * self.int_rate
+        return self
 
 
-    def deposit(self, amount):
-        # your code here
-    def withdraw(self, amount):
-        # your code here
-    def display_account_info(self):
-        # your code here
-    def yield_interest(self):
-        # your code here
+f
